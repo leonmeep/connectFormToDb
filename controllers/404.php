44d627592d4e553717fc091cce2a9ec6e6be74f3
Leon Birch <leonbirch333@gmail.com>
2024-05-14 15:04:08 +0100

    amended 404 heading

--- controllers/404.php
+++ controllers/404.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$heading = "<a href='/'><img src= /img/sg.png alt='Strange Garden'></a>";
+$heading = "404 - Page Not Found";
 $title = "404 - Page Not Found";
 
 require 'views/404.view.php';
