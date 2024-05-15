376682467b7e1835038edc5b2b3498941560e6bb
Leon Birch <leonbirch333@gmail.com>
2024-05-15 13:16:25 +0100

    styled submit button bg

--- views/response.view.php
+++ views/response.view.php
@@ -4,15 +4,15 @@ require 'views/partials/header.php';
 
 ?>
 
-    <div class="flex justify-center">
+    <div class="flex justify-center mt-64">
         <h1 class="flex text-green-400"><?= $heading ?></h1>
     </div>
 
-    <div class="flex justify-center">
+    <div class="flex justify-center mt-5">
         <p class="flex ">Your comment has been saved</p>
     </div>
 
-    <div class="flex justify-center">
+    <div class="flex justify-center mt-5">
         <a href="/" class="flex text-pink-400 hover:underline">Back to home</a>
     </div>
 
