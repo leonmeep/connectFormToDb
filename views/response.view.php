5eefe3f6331584859dd7f895ec453c1f34958596
Leon Birch <leonbirch333@gmail.com>
2024-05-15 16:18:14 +0100

    added smiley face to response page

--- views/response.view.php
+++ views/response.view.php
@@ -8,11 +8,15 @@ require 'views/partials/header.php';
         <h1 class="flex text-green-400 text-2xl p-1"><?= $heading ?></h1>
     </div>
 
+    <div class="flex justify-center mt-2">
+        <img src="/img/happy.png" alt="happy face" class="w-20 h-20">
+    </div>
+
     <div class="flex justify-center mt-5">
         <p class="flex tracking-wider text-green-400 ">Your comment has been saved</p>
     </div>
 
-    <div class="flex justify-center mt-5">
+    <div class="flex justify-center mt-10">
         <a href="/" class="flex text-pink-400 p-3 hover:underline">Return to the comments form</a>
     </div>
 
