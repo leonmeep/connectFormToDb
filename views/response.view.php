9a35f0814a6533b6d977f027d38b18c5cbb1a57f
Leon Birch <leonbirch333@gmail.com>
2024-05-15 13:43:51 +0100

    added p to response heading

--- views/response.view.php
+++ views/response.view.php
@@ -5,7 +5,7 @@ require 'views/partials/header.php';
 ?>
 
     <div class="flex justify-center mt-64">
-        <h1 class="flex text-green-400"><?= $heading ?></h1>
+        <h1 class="flex text-green-400 text-2xl p-1"><?= $heading ?></h1>
     </div>
 
     <div class="flex justify-center mt-5">
