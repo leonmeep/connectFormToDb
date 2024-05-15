b2c3bf5d37f256fe99612324661fca9f5bad94a4
Leon Birch <leonbirch333@gmail.com>
2024-05-15 17:21:28 +0100

    made username and pw dynamic

--- Database.php
+++ Database.php
@@ -5,11 +5,11 @@ class Database
 {
     public PDO $connection;
 
-    public function __construct($config)
+    public function __construct($config, $username = 'root', $password = '')
     {
         $dsn = 'mysql:' . http_build_query($config, '', ';');
 
-        $this->connection = new PDO($dsn, 'root', '', [
+        $this->connection = new PDO($dsn, $username, $password, [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         ]);
     }
