6ec9f2fe7614fe5b369b89c090e86ddb4b5f02b3
Leon Birch <leonbirch333@gmail.com>
2024-05-15 17:30:43 +0100

    changed heading on index

--- controllers/index.php
+++ controllers/index.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$heading = 'To submit a comment, please fill out this form:';
+$heading = 'To submit a comment about this site, please fill out the form below:';
 $title = 'Form';
 
 require 'views/index.view.php';
